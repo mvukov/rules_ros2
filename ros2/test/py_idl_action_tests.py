19f5409ffa937b378bb1440ffee964f3e79c75ae
Milan Vukov <mvukov@gmail.com>
2022-09-03 11:14:16 +0200

    Update tests for Python IDL

---
+++ ros2/test/py_idl_action_tests.py
@@ -0,0 +1,3 @@
+from test_action import action
+
+action.Test.__class__.__import_type_support__()
---
+++ ros2/test/py_idl_with_deps_tests.py
@@ -0,0 +1,7 @@
+from std_msgs import msg
+
+array = msg.ByteMultiArray()
+
+print(msg.ByteMultiArray())
+
+msg.ByteMultiArray.__class__.__import_type_support__()
