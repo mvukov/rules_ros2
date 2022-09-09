6d83ad2ed24e2da71a0671b030ffda6a19bef517
Milan Vukov <mvukov@gmail.com>
2022-09-09 07:53:07 +0200

    Robustify py_talker

--- examples/chatter/py_talker.py
+++ examples/chatter/py_talker.py
@@ -40,13 +40,16 @@ def main():
 
     minimal_publisher = MinimalPublisher()
 
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(minimal_publisher)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Destroy the node explicitly
+        # (optional - otherwise it will be done automatically
+        # when the garbage collector destroys the node object)
+        minimal_publisher.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
