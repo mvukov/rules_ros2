12b5252374b41b1271ae353c8870845cacfacf80
Milan Vukov <mvukov@gmail.com>
2022-09-17 21:13:57 +0200

    rm run_buildifier.sh

--- run_buildifier.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$(git rev-parse --show-toplevel)"
-buildifier -lint=warn WORKSPACE $(find . -type f -name "*.bazel") $(find . -type f -name "*.bzl")
-buildifier -lint=fix WORKSPACE $(find . -type f -name "*.bazel") $(find . -type f -name "*.bzl")
