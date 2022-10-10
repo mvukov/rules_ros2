746700a27e34c5f70f76ca7b8d6c5ecae1737e1c
Liyou Zhou <liyou@wayve.ai>
2022-10-10 13:15:01 +0100

    Fix merge errors

--- examples/actions/client.cc
+++ examples/actions/client.cc
@@ -80,9 +80,8 @@ class MinimalActionClient : public rclcpp::Node {
   rclcpp::TimerBase::SharedPtr timer_;
   bool goal_done_;
 
-  void goal_response_callback(
-      std::shared_future<GoalHandleFibonacci::SharedPtr> future) {
-    auto goal_handle = future.get();
+  void goal_response_callback(const GoalHandleFibonacci::SharedPtr & goal_handle)
+  {
     if (!goal_handle) {
       RCLCPP_ERROR(get_logger(), "Goal was rejected by server");
     } else {
