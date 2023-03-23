42ca01edaeca877537a4abd15946729ba7996f91
Alexander Hans <ahans@users.noreply.github.com>
2023-03-23 10:22:01 +0100

    Add ros2_{cc,py}_binary macros
    This adds `ros2_{cc,py}_binary` macros that are non-test versions of the
    existing `ros2_{cc,py}_test` macros. Just like the test versions, they
    differ from the `ros2_cpp_binary` and `py_binary` targets in that they
    offer the option to handle setup of ament.

---
+++ ros2/test/pluginlib/ros2_cpp_binary_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+ros2/test/pluginlib/plugin_tests_binary
---
+++ ros2/test/pluginlib/ros2_py_binary_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+ros2/test/pluginlib/py_loader_tests_binary
