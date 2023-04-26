802be1727ce4a7f4744cf74cd7a5678ef4bbfe40
Laurenz Altenmüller <laurenz.altenmueller@fernride.com>
2023-04-26 15:51:40 +0200

    fix lint

--- ros2/test/launch_pytest/rclpy_init_test.py
+++ ros2/test/launch_pytest/rclpy_init_test.py
@@ -3,13 +3,12 @@
 What is tested here is that ROS_HOME (or ROS_LOG_DIR) is set.
 If it is not set, rclpy.init() will fail to initialize.
 
-It makes sense to test this for each test driver because each uses a different wrapper
-to set the environment vars.
+It makes sense to test this for each test driver because each uses a different
+wrapper to set the environment vars.
 """
-
 import launch
-import rclpy
 import launch_testing.actions
+import rclpy
 
 
 def test_rclpy_init():
@@ -20,13 +19,11 @@ def test_rclpy_init():
 def generate_test_description():
     """This will run when using ros2_test with launch_testing as test driver."""
     rclpy.init()
-    return launch.LaunchDescription(
-        [
-            launch_testing.actions.ReadyToTest(),
-        ]
-    )
+    return launch.LaunchDescription([
+        launch_testing.actions.ReadyToTest(),
+    ])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """This will run when using ros2_py_test."""
     test_rclpy_init()
