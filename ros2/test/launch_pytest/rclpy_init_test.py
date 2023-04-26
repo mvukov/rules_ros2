083e371f8b2f7fa011d2c50f6d074014c72e03ca
Laurenz Altenmüller <laurenz.altenmueller@fernride.com>
2023-04-26 15:45:53 +0200

    Test all three Python test drivers

---
+++ ros2/test/launch_pytest/rclpy_init_test.py
@@ -0,0 +1,32 @@
+"""Test rclpy.init() in different test drivers.
+
+What is tested here is that ROS_HOME (or ROS_LOG_DIR) is set.
+If it is not set, rclpy.init() will fail to initialize.
+
+It makes sense to test this for each test driver because each uses a different wrapper
+to set the environment vars.
+"""
+
+import launch
+import rclpy
+import launch_testing.actions
+
+
+def test_rclpy_init():
+    """This will run when using ros2_test with pytest as test driver."""
+    rclpy.init()
+
+
+def generate_test_description():
+    """This will run when using ros2_test with launch_testing as test driver."""
+    rclpy.init()
+    return launch.LaunchDescription(
+        [
+            launch_testing.actions.ReadyToTest(),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    """This will run when using ros2_py_test."""
+    test_rclpy_init()
--- ros2/test/test_rclpy.py
+++
@@ -1,3 +0,0 @@
-import rclpy
-
-rclpy.init()
