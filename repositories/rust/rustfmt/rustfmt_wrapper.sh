fa543692c750e187fa9e47148e8c2c991c4ae072
Milan Vukov <mvukov@gmail.com>
2025-02-09 22:15:29 +0100

    Wrap up rustfmt_wrapper.sh

--- repositories/rust/rustfmt/rustfmt_wrapper.sh
+++ repositories/rust/rustfmt/rustfmt_wrapper.sh
@@ -2,4 +2,11 @@
 
 set -o errexit -o nounset
 
-$(dirname "$0")/rustfmt_wrapper_impl.sh --config-path=`pwd` "$@"
+impl=$(dirname "$0")/rustfmt_wrapper_impl.sh
+
+if [ ! -f "${impl}" ]; then
+    echo "rustfmt is not set up. Please run ./repositories/rust/rustfmt/generate.sh"
+    exit 1
+fi
+
+"${impl}" --config-path=`pwd` "$@"
