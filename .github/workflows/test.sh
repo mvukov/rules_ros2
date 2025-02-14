f880154780111d5d609e276a3fc8e5cb710ff8f6
Alexander Hans <ahans@users.noreply.github.com>
2025-02-14 18:15:04 +0100

    Include build of external repos in CI

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -8,3 +8,6 @@ if [[ ! -z "${BUILDBUDDY_ORG_API_KEY}" ]]; then
 fi
 bazel --bazelrc=$GITHUB_WORKSPACE/.github/workflows/ci.bazelrc --bazelrc=.bazelrc \
     test "${BAZEL_REMOTE_CONFIG[@]}" //... "$@"
+
+bazel --bazelrc=$GITHUB_WORKSPACE/.github/workflows/ci.bazelrc --bazelrc=.bazelrc \
+    build "${BAZEL_REMOTE_CONFIG[@]}" $(bazel query '//external:*' | grep ":ros2_" | sed 's#//external:\(ros2_.*\)#\@\1//...#') "$@"
