2e97f65d5cada0df36cf223ec7d4a7a4d90e3ed3
Yuki Furuta <y.furuta@gitai.tech>
2026-04-20 21:37:58 -0700

    feat: add ros2 interface CLI command support
    Add Bazel macro and Python wrapper to enable ros2 interface command
    functionality within Bazel-built ROS 2 projects.
    
    Changes:
    - Add interface.bzl macro for creating ros2_interface binary targets
    - Add ros2_interface.py Python wrapper exposing interface CLI verbs
    - Register new files in ros2/BUILD.bazel exports_files

---
+++ ros2/ros2_interface.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+import ros2cli.cli
+import ros2interface.verb.list
+import ros2interface.verb.package
+import ros2interface.verb.packages
+import ros2interface.verb.proto
+import ros2interface.verb.show
+
+import ros2.ros2_cmd
+
+COMMAND_EXTENSIONS = {
+    'list': ros2interface.verb.list.ListVerb(),
+    'package': ros2interface.verb.package.PackageVerb(),
+    'packages': ros2interface.verb.packages.PackagesVerb(),
+    'proto': ros2interface.verb.proto.ProtoVerb(),
+    'show': ros2interface.verb.show.ShowVerb(),
+}
+
+extension = ros2.ros2_cmd.Ros2CommandExtension(COMMAND_EXTENSIONS)
+sys.exit(ros2cli.cli.main(argv=sys.argv[1:], extension=extension))
